ae2a7c5fa1447370c3712449774034101c29454d
arhumbert1978 <72741079+arhumbert1978@users.noreply.github.com>
2021-07-12 11:53:49 +0200

    rgret
    erter

---
+++ Le_Financier/Banque.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Le_Financier
+{
+    public class Banque 
+    {
+        //attributs
+        private int nbCompte;
+        private Compte[] listComptes;
+        
+
+
+        private string nomBanque;
+        private string ville;
+
+        public Banque(string _nom, string _ville)
+        {
+
+
+            this.nomBanque = _nom;
+            this.ville = _ville;
+            listComptes = new Compte[50];
+            this.nbCompte = 0;
+        }
+
+        private void AddComptes(Compte _unCompte)
+        {
+
+            this.listComptes[nbCompte] = _unCompte;
+            nbCompte++;
+
+        }
+
+
+        public override string ToString()
+        {
+            string MesComptes = " Les differents comptes de la banque " + this.nomBanque + " basé à " + this.ville + " sont :\n";
+
+            for (int i = 0; i < this.nbCompte; i++)
+            {
+                MesComptes += this.listComptes[i].ToString() + "\n";
+                MesComptes+=("\n"+"------------------*************---------------");
+            
+            }
+
+
+
+            return MesComptes;
+            
+        }
+    }
+    }
+
+
